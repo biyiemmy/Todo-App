5f74d3eaf2b96148b335caff0ceeb138eeac429f
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-08-03 21:39:35 +0100

    add new line

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -17,7 +17,7 @@ export const Time = () => {
     <div className="footer fixed bottom-0 text-center bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 p-4 rounded-lg shadow-md">
       <h4 className="text-lg font-medium">Clock: {time}</h4>
       <h5 className="mt-2 text-sm">
-        Time wait for nodbody, <br /> Oya start your task(s)
+        Time wait for nodbody, <br /> Oya start your task
       </h5>
     </div>
   );
